b41e7fae0615617459558d4c4dd3aa41858f5bdc
mynamedoesntfi <darthmarth550@gmail.com>
2016-05-20 10:49:25 -0700

    Add files via upload

--- getPoints.m
+++ getPoints.m
@@ -1,12 +1,29 @@
-function [output] = getPoints(str,xmin,xmax)
+function getPoints(str,xmin,xmax)
 {
   f = inline(str);      % turn input into function of x 
-  z = inline('abs(f(x))');
-  zmax = num2str(z(xmax));
-  y = inline(['z(x)-' zmax]);
-  f(1);
-  z(1);
-  f(1);  
+  z = inline('-1*abs(f(x))');
+  zmax = num2str(abs(f(xmax)));
+  y = inline(['z(x)+' zmax]);
+  
+  %% Range
+  ymax = y(xmin);
+  ymin = y(xmax);
+  range = ymax - ymin;
+  numLayers = range / 0.6;
+  numLayers = int16(numLayers);
+  layer = 0;
+  
+  while layer < numLayers
+  {
+    for n = 0:2
+    {
+      ycoord = 0.6(layer);
+      
+     
+    }
+    
+    layer = layer + 1;
+  
   
   
 }
