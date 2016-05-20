8e6a8150cc7cc20c28c870ac8bc0ddf0efbaa0ba
mynamedoesntfi <darthmarth550@gmail.com>
2016-05-20 11:37:02 -0700

    Add files via upload

--- getPoints.m
+++ getPoints.m
@@ -1,5 +1,5 @@
 function getPoints(str,xmin,xmax)
-{
+
   f = inline(str);      % turn input into function of x 
   z = inline('-1*abs(f(x))');
   zmax = num2str(abs(f(xmax)));
@@ -12,19 +12,17 @@ function getPoints(str,xmin,xmax)
   numLayers = range / 0.6;
   numLayers = int16(numLayers);
   layer = 0;
-  
-  while layer < numLayers
-  {
+  fid = fopen('gcode.txt','w');
+  while (layer < numLayers)
     for n = 0:2
-    {
-      ycoord = 0.6(layer);
-      
-     
-    }
-    
-    layer = layer + 1;
+      ycoord = 0.6 * layer;
+      xcoord = fzero(@(x) y(x)-ycoord,[0,100]);
+      printf("The y coordinate is %f, the x coordinate is %f\n",ycoord,xcoord);
+    endfor
+    layer = layer + 1;  
+  endwhile
   
   
   
-}
+
 endfunction
