07893adc7aaa878a5456fddf83ba93ab416ed997
MasterofJavaCode <sandra.sa800@gmail.com>
2016-05-23 10:59:37 -0700

    Update getPoints.m

--- getPoints.m
+++ getPoints.m
@@ -8,27 +8,33 @@ function getPoints(str,xmin,xmax)
   %% Range
   ymax = y(xmin);
   ymin = y(xmax);
-  range = ymax - ymin;
+  range = (ymax - ymin)*10;
   numLayers = range / 0.6;
   numLayers = int16(numLayers);
+  extrusion = 2.5;
+  
   printf('numLayers is %f, ymin is %f, ymax is %f\n', numLayers, ymin, ymax);
-  layer = 0;
+  layer = 1;
   fid = fopen('gcode.txt','w');
   gsetup = 'G21 ; set units to millimeters\nM107\nM190 S55 ; wait for bed temperature to be reached\nM104 S196 ; set temperature\nG28 ; home all axes \nG1 Z5 F5000 ; lift nozzle \nM109 S196 ; wait for temperature to be reached \nG90 ; use absolute coordinates\nG92 E0\nM83 ; use realtive distances for extrusion\n';
-  printf(gsetup); 
-  while (layer < numLayers)
+  fprintf(gsetup); 
+  
+  while (layer < (numLayers))
+    fprintf('G1 E1.00000 F1800.000\nG92 E0\n');
     ycoord = 0.6 * layer;
-    xcoord = fzero(@(x) y(x)-ycoord,[0,100]);
+    xcoord = fzero(@(x) y(x)*10-ycoord,[0,100]);
+    fprintf('G1 Z%f\n', ycoord)
     for n = 0:2
       if ((xcoord-0.2) > 0)
       xcoord = xcoord - 0.2*n;
+      printf("G02 X%f Y%f I%f J%f E%f\n",xcoord,0,-xcoord,0, extrusion);
       endif
-      printf("The y coordinate is %f, the x coordinate is %f\n",ycoord,xcoord);
+      
     endfor
     layer = layer + 1;  
   endwhile
   gfinishup = 'G92 E0 \nM104 S0 ; turn off temperature \nG28 X0  ; home X axis \nM84  ; disable motors\n';
-  printf(gfinishup);
+  fprintf(gfinishup);
   
   
 
