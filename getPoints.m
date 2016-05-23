c172ce7c010d9269a8437362def4f2c22e416456
mynamedoesntfi <darthmarth550@gmail.com>
2016-05-23 10:56:53 -0700

    Add files via upload

--- getPoints.m
+++ getPoints.m
@@ -10,14 +10,22 @@ function getPoints(str,xmin,xmax)
   ymin = y(xmax);
   range = ymax - ymin;
   numLayers = range / 0.6;
-  numLayers = int16(numLayers);
+%  numLayers = int16(numLayers);
   layer = 0;
-  fid = fopen('gcode.txt','w');
-  while (layer < numLayers)
+  fid = fopen("gcode.txt",'w+t');
+  inString = {"G21 ; set units to millimeters","M107","M190 s55","M104 s196","G28","G1 z5 F5000","M109 s196","G90","G92 E0","M82"};
+  for i=1:size(inString)
+    fprintf(fid,"%s\n",inString{1,i});
+  endfor
+  while (layer <= numLayers)
     for n = 0:2
       ycoord = 0.6 * layer;
-      xcoord = fzero(@(x) y(x)-ycoord,[0,100]);
-      printf("The y coordinate is %f, the x coordinate is %f\n",ycoord,xcoord);
+      xcoord = (fzero(@(x) y(x)-ycoord,[0,100])) - (0.5 * n);
+      
+      
+%      out = printf("The y coordinate is %f, the x coordinate is %f\n",ycoord,xcoord);
+%      out = printf("G01 X%f Y0 Z%f",xcoord,ycoord);
+%      out = printf("G02 X%f Y0 Z%f I0 J0 K%F",xcoord,ycoord,ycoord);
     endfor
     layer = layer + 1;  
   endwhile
