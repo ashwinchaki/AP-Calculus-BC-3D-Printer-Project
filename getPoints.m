3d173d3ffc04d5fac59f477dfa6415ba38de1522
MasterofJavaCode <sandra.sa800@gmail.com>
2016-05-26 10:53:35 -0700

    Add files via upload

--- getPoints.m
+++ getPoints.m
@@ -13,29 +13,29 @@ function getPoints(str,xmin,xmax)
   numLayers = int16(numLayers);
   extrusion = 2.5;
   
-  printf('numLayers is %f, ymin is %f, ymax is %f\n', numLayers, ymin, ymax);
-  layer = 1;
+
+  layer = 0;
   fid = fopen('gcode.txt','w');
   gsetup = 'G21 ; set units to millimeters\nM107\nM190 S55 ; wait for bed temperature to be reached\nM104 S196 ; set temperature\nG28 ; home all axes \nG1 Z5 F5000 ; lift nozzle \nM109 S196 ; wait for temperature to be reached \nG90 ; use absolute coordinates\nG92 E0\nM83 ; use realtive distances for extrusion\n';
-  fprintf(gsetup); 
+  fputs(gsetup); 
   
   while (layer < (numLayers))
     fprintf('G1 E1.00000 F1800.000\nG92 E0\n');
     ycoord = 0.6 * layer;
     xcoord = fzero(@(x) y(x)*10-ycoord,[0,100]);
-    fprintf('G1 Z%f\n', ycoord)
     for n = 0:2
       if ((xcoord-0.2) > 0)
       xcoord = xcoord - 0.2*n;
-      printf("G02 X%f Y%f I%f J%f E%f\n",xcoord,0,-xcoord,0, extrusion);
+      fputs("G01 X%f Y0 Z%f\n",xcoord+100,ycoord+100);
+      fputs("G02 X%f Y0 Z%f I%f J0 K%f E%f\n",xcoord+100,ycoord+100, -xcoord-100,-ycoord-100,extrusion);
       endif
       
     endfor
     layer = layer + 1;  
   endwhile
   gfinishup = 'G92 E0 \nM104 S0 ; turn off temperature \nG28 X0  ; home X axis \nM84  ; disable motors\n';
-  fprintf(gfinishup);
-  
+  fputs(gfinishup);
+  fclose(fid);
   
 
 endfunction
